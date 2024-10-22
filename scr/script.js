35c7fd2ea82c596f0b55c44a6a69bf81b1288452
Klara Holm <3ggjs24holmkl@folkuniversitetet.nu>
2024-10-22 09:33:56 +0200

    La till förslag på globala variabler

---
+++ scr/script.js
@@ -0,0 +1,9 @@
+//Min gissning är att vi kommer vilja ha dessa som globala variabler?
+
+let deck //dumt ifall den finns både som global variabel och som lokal i generateDeck-funktionerna
+
+let playerHand //Skulle behöva annat namn ifall vi har fler spelare
+let dealerHand
+
+let playerScore
+let dealerScore //Inte så bra namn ifall man både räknar summan av handen och hur många rundor som vunnits
